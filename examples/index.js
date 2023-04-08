280fefe7c05b1a1fac5cdca01e41fb0fa4075bab
Akash Kumar Verma <36815360+ultimateakash@users.noreply.github.com>
2023-04-08 16:57:39 +0530

    puppeteer-html-pdf@v1.0.0

---
+++ examples/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const util = require('util');
+const htmlPDF = require('../lib');
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
+const createPDF = async () => {
+  const pdfContext =  {
+    invoiceItems: [
+      { item: 'Website Design', amount: 5000 },
+      { item: 'Hosting (3 months)', amount: 2000 },
+      { item: 'Domain (1 year)', amount: 1000 },
+    ],
+    invoiceData: {
+      invoice_id: 123,
+      transaction_id: 1234567,
+      payment_method: 'Paypal',
+      creation_date: '04-05-1993',
+      total_amount: 141.5,
+    },
+    baseUrl: 'https://ultimateakash.com'
+  }
+
+  const options = {
+    format: 'A4',
+    context: pdfContext
+  }
+
+  // const content = '<h1>Welcome to puppeteer-html-pdf</h1>';
+  // const content = 'https://www.google.com'; 
+  const content  = await readFile(__dirname + '/sample.html','utf8'); 
+    
+  try {
+    const buffer = await htmlPDF.create(content, options); 
+    await writeFile('sample.pdf', buffer)
+  } catch (error) {
+    console.log('htmlPDF error', error)
+  }
+}
+
+createPDF();
