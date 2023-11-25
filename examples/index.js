f3dbaa816f90175b3e672280445aae7390f55fe8
Akash Kumar Verma <36815360+ultimateakash@users.noreply.github.com>
2023-11-25 19:54:32 +0530

    Update examples

--- examples/index.js
+++ examples/index.js
@@ -4,27 +4,38 @@ const hbs = require('handlebars');
 const createPDF = async () => {
   const htmlPDF = new PuppeteerHTMLPDF();
   const options = {
-    format: 'A4'
+    format: 'A4',
+    // path: `${__dirname}/sample.pdf` 
   }
-  htmlPDF.setOptions(options); 
+  htmlPDF.setOptions(options);
 
-  const content1 = 'https://www.google.com'; 
-  const content2 = 'https://www.yahoo.com'; 
-    
+  const pdfData =  {
+    invoiceItems: [
+      { item: 'Website Design', amount: 5000 },
+      { item: 'Hosting (3 months)', amount: 2000 },
+      { item: 'Domain (1 year)', amount: 1000 },
+    ],
+    invoiceData: {
+      invoice_id: 123,
+      transaction_id: 1234567,
+      payment_method: 'Paypal',
+      creation_date: '04-05-1993',
+      total_amount: 141.5,
+    },
+    baseUrl: 'https://ultimateakash.com'
+  }
+  // const content = "<style> h1 {color:red;} </style> <h1>Welcome to puppeteer-html-pdf</h1>";
+  // const content = 'https://www.google.com'; 
+  const html = await htmlPDF.readFile(__dirname + '/sample.html','utf8');  
+  const template = hbs.compile(html);
+  const content = template(pdfData);
+
   try {
-    htmlPDF.setAutoCloseBrowser(false)
-    const pdfBuffer1 = await htmlPDF.create(content1); 
-    const filePath1 = `${__dirname}/google.pdf`
-    await htmlPDF.writeFile(pdfBuffer1, filePath1);
-
-    const pdfBuffer2 = await htmlPDF.create(content2); 
-    const filePath2 = `${__dirname}/yahoo.pdf`
-    await htmlPDF.writeFile(pdfBuffer2, filePath2);
+    const pdfBuffer = await htmlPDF.create(content); 
+    const filePath = `${__dirname}/sample.pdf`
+    await htmlPDF.writeFile(pdfBuffer, filePath);
   } catch (error) {
     console.log('PuppeteerHTMLPDF error', error);
-  }
-  finally {
-    await htmlPDF.closeBrowser();
   }
 }
 
